1296dd4c84b3a6bb997b1369ad92d3afe6021f63
rueng564 <rueng564@naver.com>
2020-11-22 00:26:21 +0900

    주석 제거

--- src/sort/QuickSort.js
+++ src/sort/QuickSort.js
@@ -35,47 +35,6 @@ class QuickSort extends Sort {
         arr[i].color = arr[j].color = Color.WHITE;
         setTimeout(() => this.position(arr, left, right, i, j, pivot), this.delay);
     }
-
-    /*QuickSort = (delay, length) => {
-        let timeidx = 0;
-        const heightList = this.state.heightList.slice();
-        const quickSort = (arr, left, right) => {
-            if (left < right) {
-                const i = position(arr, left, right, timeidx++);
-                quickSort(arr, left, i - 1);
-                quickSort(arr, i + 1, right);
-            }
-            return arr;
-        };
-
-        const position = (arr, left, right, timei) => {
-            let i = left;
-            let j = right;
-            let pivot = arr[left];
-            let changeQueue = [];
-
-            while (i < j) {
-                while (arr[j].height > pivot) j--;
-                while (i < j && arr[i].height <= pivot) i++;
-                let tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-                changeQueue.push(arr.slice());
-            }
-            arr[left] = arr[j];
-            arr[j] = pivot;
-            changeQueue.push(arr.slice());
-            let additionalDelay = delay / changeQueue.length;
-            let additionalDelayidx = 0;
-            for (const arr of changeQueue) {
-                const idx = this.updateHeightList(arr, timei * delay + (additionalDelayidx++) * additionalDelay);
-                this.DelayList.push(idx);
-            }
-            return j;
-        }
-        quickSort(heightList, 0, length - 1);
-        return timeidx;
-    }*/
 }
 
 export default QuickSort;
